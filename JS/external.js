1db837777a38e98971c86f725a8d20787fd5883e
CleoBreaux <corey.osbey@gmail.com>
2022-07-28 14:21:25 -0500

    my notes

---
+++ JS/external.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var myAge = 24;
+console.log(myAge);
+alert("this is my external file");
+alert("this is my second alert");
+var isStudent = confirm("click okay if you Are you a student");
+console.log(isStudent);
+var firstName = prompt("What is your first name")
+var lastname = prompt("what is your last name")
