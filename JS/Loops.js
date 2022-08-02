c15d407aa97703ca400a2bf406ca4bcb8444680f
CleoBreaux <corey.osbey@gmail.com>
2022-08-02 13:35:33 -0500

    conditionals.html

---
+++ JS/Loops.js
@@ -0,0 +1,5 @@
+"use script";
+
+for(var num = 0; num <= 100; num+= 5){
+    console.log(num)
+}
--- conditionals.js
+++ conditionals.js
@@ -17,5 +17,53 @@ function analyzeColor(color) {
     } else if (color === "violet") {
         console.log(" I don't know anything that is the color violet.")
     }
-}
-analyzeColor(userColor)
+} analyzeColor(userColor)
+    switch(color){
+        case "red":
+            return: "stop signs are red"
+        case "blue":
+            return "blue jays are blue"
+        case "green":
+            return "grass is green"
+        case "orange"
+            return "tangerines are orange"
+        case "indigo":
+            return "I dont know anything the color of indigo"
+        case "violet":
+            return "I dont know anything the color violet"
+
+
+            function calculateTotal(luckyNum, total) {
+                switch (luckyNum) {
+                    case 0: return total
+                    case 1: return total - (total * .10)
+                    case 2: return total - (total * .25)
+                    case 3: return total - (total * .35)
+                    case 4: return total - (total * .50)
+                    case 5: return total - total
+                }
+            }
+
+    }
+
+    var userNum = confirm("would you like to enter a number ?");
+    if(userNum === true){
+        var thatNumber = prompt("give me that number");
+        if(isNaN(thatNumber))
+            alert("this is not a number")
+        if(thatNumber %2 === 0){
+            alert("that number sure is even")
+        }else if(thatNumber % 2 !== 0){
+            alert("that number sure is odd to me")
+        }
+        var newNum = thatNumber + 100;
+        alert(`${thatNumber} + 100 = ${newNum}`);
+        if(thatNumber < 0) {
+            alert("that's a negative number");
+        }else if(thatNumber > 0){
+            alert(`this number is positive ${thatNumber}`);
+
+        }else if(thatNumber < 0){
+            alert(`this number is negative ${thatNumber}`);
+        }
+    }
