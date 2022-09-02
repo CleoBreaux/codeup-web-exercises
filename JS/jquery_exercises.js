e7d9da803c92aa835ff0fad6d97c38d63305752f
CleoBreaux <corey.osbey@gmail.com>
2022-09-02 16:54:18 -0500

    jquery selectors exercise

--- JS/jquery_exercises.js
+++ JS/jquery_exercises.js
@@ -1,3 +1,6 @@
-$(function () {
-    alert('THe DOM has finished loading!')
-});
+$('.codeup').css('border', '1px solid #FE2D00');
+$('li').css('font-size','20px');
+$("li, p,h1 ").css('background-color', '#FEF600');
+$(function() {
+alert($('h1').html())
+})
