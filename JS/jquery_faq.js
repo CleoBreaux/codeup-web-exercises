b69cf0a5b518e1e6ae360b07b958e2f7f18340e6
CleoBreaux <corey.osbey@gmail.com>
2022-09-07 16:50:55 -0500

    traversing exercise

--- JS/jquery_faq.js
+++ JS/jquery_faq.js
@@ -4,6 +4,19 @@ $(".invisible").css("visibility","hidden")
 $('#showButton').click(function (){
     $('dd').toggleClass(".invisible").css("visibility","visible")
 });
-// $("dd").addClass("invisible");
-// $(".invisible").css("visibility", 'hidden')
-// $(document).toggleClass('dd').on()
+// end of FaQ exercise
+
+$('#highlightButton').click(function (){
+ $(".last").toggleClass(".last").css("background-color", "yellow")
+});
+
+$("h3").click(function(){
+     $("ul").each(function () {
+         $(this).children().css('font-weight', 'bold')
+     })
+     });
+
+$("li").on('click',function(){
+   $(this).parent().children().first().css('color', 'blue')
+});
+
